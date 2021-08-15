07be0e443505461f82038e0e7a2d092d17f69a10
Parul Verma <verma.parul14799@gmail.com>
2021-08-15 15:15:21 +0530

    Featured products

--- src/app/secure/product-list/product-list.component.ts
+++ src/app/secure/product-list/product-list.component.ts
@@ -7,9 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
+  featuredProducts = [
+    {
+      url:'../../../assets/images/product1.jpg',
+      name: 'Aliquam Quaerat',
+      price:'₹108.80'
+    },
+    {
+      url:'../../../assets/images/product2.jpg',
+      name: 'Loroum Eveniet',
+      price:'₹97.99'
+    },{
+      url:'../../../assets/images/product3.jpg',
+      name: 'Necessitiatibus',
+      price:'₹119.60'
+    },{
+      url:'../../../assets/images/product4.jpg',
+      name: 'Commodi Conseqatur',
+      price:'₹134.00'
+    },{
+      url:'../../../assets/images/product5.jpg',
+      name: 'Aliquam Quaerat',
+      price:'₹108.80'
+    },{
+      url:'../../../assets/images/product6.jpg',
+      name: 'Exercitat Virginia',
+      price:'₹104.00'
+    },{
+      url:'../../../assets/images/product7.jpg',
+      name: 'Laudant Doloremque',
+      price:'₹92.00'
+    },{
+      url:'../../../assets/images/product8.jpg',
+      name: 'Nostrud Exercitation',
+      price:'₹78.80'
+    },
+  ]
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
+
+
 
 }
